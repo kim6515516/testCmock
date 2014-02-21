2e200660736e5d6a866598c545e8f18aa03ee844
kim6515516 <kim6515516@kim6515516-VirtualBox.(none)>
2014-02-21 15:46:14 +0900

    modified:   KimMyeungHoe/src/Main.c 	deleted:    KimMyeungHoe/src/main.c

--- KimMyeungHoe/src/Main.c
+++ KimMyeungHoe/src/Main.c
@@ -8,12 +8,10 @@ void main(void)
     if (pDiskSpaceStatList == NULL) {
         printf("failed to get a infomation of filesystem\n");
     }
-    
     if (pDiskSpaceStatList->terrno == 1) {    	
-	    printf("failed to peopen() \n");
-	}
-	
+        printf("failed to peopen() \n");
+    }
     if (pDiskSpaceStatList->terrno == 2) {    	
-	    printf("failed to excute a commnd \n");
-	}
+        printf("failed to excute a commnd \n");
+    }
 }
